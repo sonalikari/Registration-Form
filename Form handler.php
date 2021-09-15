184941221d1875e5460c0449b8a69057482c4ab4
sonalikari <sonali211103@gmail.com>
2021-09-15 23:24:52 +0530

    First commit

---
+++ Form handler.php
@@ -0,0 +1,17 @@
+<?php
+$name=$_POST['name']; 
+$visitor_email=$_POST['email']; 
+$phone=$_POST['phone']; 
+$message=$_POST['message']; 
+$email_from='sonalirai161161@gmail.com'; 
+$email_subject="New Form Submission"; 
+$email_body="User Name:$name.\n". 
+ "User Email:$visitor_email.\n". 
+ "User Phone:$phone.\n". 
+ "User Message:$message.\n"; 
+$to="sonalirai161161@gmail.com"; 
+$headers="From:$email_from\r\n"; 
+$headers.="Reply-To:$visitor_email\r\n"; 
+mail($to,$email_subject,$email_body,$headers); 
+header("Location:Contact.html"); 
+?>
