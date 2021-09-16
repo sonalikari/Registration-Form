60e3b6e82c2f03f5a7a84fa3bc53ccc3f52a0072
sonalikari <90141326+sonalikari@users.noreply.github.com>
2021-09-16 18:32:50 +0530

    Updated php

--- Form handler.php
+++ Form handler.php
@@ -2,16 +2,16 @@
 $name=$_POST['name']; 
 $visitor_email=$_POST['email']; 
 $phone=$_POST['phone']; 
-$message=$_POST['message']; 
+$Course type=$_POST['course type']; 
 $email_from='yahoo123@gmail.com'; 
 $email_subject="New Form Submission"; 
 $email_body="User Name:$name.\n". 
  "User Email:$visitor_email.\n". 
  "User Phone:$phone.\n". 
- "User Message:$message.\n"; 
+ "User Course type:$course type.\n"; 
 $to="sonalirai161161@gmail.com"; 
 $headers="From:$email_from\r\n"; 
 $headers.="Reply-To:$visitor_email\r\n"; 
 mail($to,$email_subject,$email_body,$headers); 
-header("Location:Contact.html"); 
+header("Location:Index.html"); 
 ?>
