70e2a8a9f1ca5c900c07189ed5386a21f0795667
Sonali Kumari <90141326+sonalikari@users.noreply.github.com>
2021-10-08 18:55:51 +0530

    Updated php

--- Form handler.php
+++ Form handler.php
@@ -3,7 +3,7 @@ $name=$_POST['name'];
 $visitor_email=$_POST['email']; 
 $phone=$_POST['phone']; 
 $course type=$_POST['course type']; 
-$email_from='yahoo123@gmail.com'; 
+$email_from='yahoo202024@gmail.com'; 
 $email_subject="New Form Submission"; 
 $email_body="User Name:$name.\n". 
  "User Email:$visitor_email.\n". 
@@ -13,5 +13,5 @@ $to="sonalirai161161@gmail.com";
 $headers="From:$email_from\r\n"; 
 $headers.="Reply-To:$visitor_email\r\n"; 
 mail($to,$email_subject,$email_body,$headers); 
-header("Location:Index.html"); 
+header("Location:index.html"); 
 ?>
