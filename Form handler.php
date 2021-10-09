99bdaae0179421156bc53dbaf2eccfa4b4bb4400
Sonali Kumari <90141326+sonalikari@users.noreply.github.com>
2021-10-09 08:17:56 +0530

    Updated php

--- Form handler.php
+++ Form handler.php
@@ -1,17 +1,18 @@
 <?php
-$name=$_POST['name']; 
-$visitor_email=$_POST['email']; 
-$phone=$_POST['phone']; 
-$course type=$_POST['course type']; 
-$email_from='yahoo202024@gmail.com'; 
-$email_subject="New Form Submission"; 
-$email_body="User Name:$name.\n". 
- "User Email:$visitor_email.\n". 
- "User Phone:$phone.\n". 
- "User Course type:$course type.\n"; 
-$to="sonalirai161161@gmail.com"; 
-$headers="From:$email_from\r\n"; 
-$headers.="Reply-To:$visitor_email\r\n"; 
+$name = $_POST['name']; 
+$visitor_email = $_POST['email']; 
+$phone = $_POST['phone']; 
+$course = $_POST['course']; 
+$email_from = 'example@gmail.com'; 
+$email_subject = "New Form Submission"; 
+$email_body = "User Name: $name.\n". 
+            "User Email: $visitor_email.\n". 
+            "User Phone: $phone.\n". 
+            "User Course : $course.\n"; 
+$to = "sonalirai161161@gmail.com"; 
+
+$headers = "From: $email_from\r\n"; 
+$headers .= "Reply-To: $visitor_email\r\n"; 
 mail($to,$email_subject,$email_body,$headers); 
-header("Location:index.html"); 
+header("Location: index.html"); 
 ?>
